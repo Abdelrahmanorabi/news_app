874470486b05e6f3e01389aa2a355f04de012f54
AbdelrahmanDev <abdelrahmanoraby101@gmail.com>
2022-08-31 01:55:09 +0200

    init news  app project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (_) => HomeScreen()},
+    );
+  }
+}
